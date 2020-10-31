14c35b04a9435eb48c4e442252b40af1b2a97905
Saif Hashmi <saifhashmi008@gmail.com>
2020-11-01 02:12:57 +0500

    text changed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Changes by Saif Hashmi 2017403!
         </p>
         <a
           className="App-link"
